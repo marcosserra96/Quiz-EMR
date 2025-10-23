dd3a51579e39734754ea0dead00ed24592d09bca
marcosserra96 <marcospauloserra@outlook.com.br>
2025-10-23 12:49:17 -0300

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,14 +1,13 @@
-/* ----------------------------------------------
-   Conexão com o Firebase - Quiz EMR
-   ---------------------------------------------- */
+// ----------------------------------------------
+// Firebase Firestore - compatível com GitHub Pages
+// ----------------------------------------------
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+  getFirestore, doc, getDoc, setDoc, addDoc, updateDoc, deleteDoc,
+  collection, getDocs, onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Importa módulos necessários (compatíveis com GitHub Pages via CDN)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, get, remove, update } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-
-/* ----------------------------------------------
-   Configuração do Firebase
-   ---------------------------------------------- */
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC2l8LU3vYfQjTly8JSa658mfIlVk2Dw8E",
   authDomain: "inovacao-emr.firebaseapp.com",
@@ -16,26 +15,16 @@ const firebaseConfig = {
   storageBucket: "inovacao-emr.firebasestorage.app",
   messagingSenderId: "1075399271811",
   appId: "1:1075399271811:web:f532f25547125d6a8f42b6",
-  measurementId: "G-8CTLMNCZJN",
-  databaseURL: "https://inovacao-emr-default-rtdb.firebaseio.com/"
+  measurementId: "G-8CTLMNCZJN"
 };
 
-/* ----------------------------------------------
-   Inicialização
-   ---------------------------------------------- */
+// Inicialização
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getFirestore(app);
 
-/* ----------------------------------------------
-   Exports para outros scripts
-   ---------------------------------------------- */
+// Exporta
 export {
   db,
-  ref,
-  set,
-  push,
-  onValue,
-  get,
-  remove,
-  update
+  doc, getDoc, setDoc, addDoc, updateDoc, deleteDoc,
+  collection, getDocs, onSnapshot
 };
